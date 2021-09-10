90a7d62f4774c4b4fb60615fe4fdeb2e433dc59e
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-09-10 08:49:14 +0700

    Update BaseController.java

--- src/main/java/com/template/demo/controller/BaseController.java
+++ src/main/java/com/template/demo/controller/BaseController.java
@@ -10,10 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import com.template.demo.model.*;
 import com.template.demo.dao.*;
+import org.springframework.context.annotation.*;
+
 
 
 /**
